72c3e8639bb60bb7c370d1ef8d3d3207247f27a1
aixa-source <aixa8868@gmail.com>
2025-12-17 09:26:05 -0600

    AREA TRIANGULO MILDRED AZUL CARDENAS LOPEZ

--- area_triangulo.py
+++ area_triangulo.py
@@ -1,8 +1,7 @@
-def triangulo():
-  lada1=float(input("ingresa el lado 1 del triangulo"))
-  lado2=float(input("ingresa el lado 2 del  triangulo"))
-  lado3=float(input("ingresa el lado 3 del triangulo"))
+def calcular_area_triangulo():
+    base = float(input("Base: "))
+    altura = float(input("Altura: "))
+    return (base * altura) / 2
 
-  perimetro=(lada1+lado2+lado3)
-  print("EL PERIMETRO DEL TRIANGULO ES: ",perimetro)
-triangulo()
+area = calcular_area_triangulo()
+print("Área del triángulo:", area)
