74e1e38af3b280c6243498973d13cdffd94b3e8c
dag18575-svg <lopezadair249@gmail.com>
2025-12-17 10:28:39 -0600

    area_cuadrado_omar

---
+++ area_cuadrado.py
@@ -0,0 +1,7 @@
+def area_cuadrado(lado):
+    return lado * lado
+
+lado = float(input("Ingresa el valor del lado del cuadrado: "))
+area = area_cuadrado(lado)
+
+print("El área del cuadrado es:", area)
